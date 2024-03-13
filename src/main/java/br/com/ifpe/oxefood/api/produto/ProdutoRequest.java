bea2983587de612ead85b9b53a52e0fbb87016a1
aluno <aluno@ifpe>
2024-03-13 18:01:11 -0300

    exercicio

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
@@ -1,5 +1,48 @@
 package br.com.ifpe.oxefood.api.produto;
 
+import java.time.LocalDate;
+
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import br.com.ifpe.oxefood.modelo.produto.Produto;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProdutoRequest {
-    
+      @Column
+   private String codigo;
+
+   @Column
+   private String titulo;
+
+   @Column
+   private String descricao;
+
+   @Column
+   private double valorUnitario;
+
+   @Column
+   private int tempoEntregaMinimo;
+
+   @Column
+   private int tempoEntregaMaximo;
+
+   public Produto build() {
+
+       return Produto.builder()
+           .codigo(codigo)
+           .titulo(titulo)
+           .descricao(descricao)
+           .valorUnitario(valorUnitario)
+           .tempoEntregaMinimo(tempoEntregaMinimo)
+           .tempoEntregaMaximo(tempoEntregaMaximo)
+           .build();
+   }
+
 }
