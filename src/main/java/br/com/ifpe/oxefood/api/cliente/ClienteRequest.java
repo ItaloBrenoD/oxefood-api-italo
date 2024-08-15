6fcc82dfda1124334feb8c091fdc52e7156376ed
aluno <aluno@ifpe>
2024-08-15 16:31:09 -0300

    teste

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
@@ -1,13 +1,16 @@
 package br.com.ifpe.oxefood.api.cliente;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import br.com.ifpe.oxefood.model.acesso.Usuario;
 import br.com.ifpe.oxefood.model.cliente.Cliente;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -21,8 +24,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClienteRequest {
+    @NotBlank(message = "O e-mail é de preenchimento obrigatório")
+    @Email
+    private String email;
 
-    //@NotBlank = verifica se é nulo e vazio
+    @NotBlank(message = "A senha é de preenchimento obrigatório")
+    private String password;
+
+    // @NotBlank = verifica se é nulo e vazio
     @NotNull(message = "O nome é de preenchimento obrigatório")
     @NotEmpty(message = "O nome é de preenchimento obrigatório")
     @Length(max = 100, message = "O nome deverá ter no máximo 100 caracteres")
@@ -42,13 +51,22 @@ public class ClienteRequest {
 
     public Cliente build() {
 
-       return Cliente.builder()
-           .nome(nome)
-           .dataNascimento(dataNascimento)
-           .cpf(cpf)
-           .foneCelular(foneCelular)
-           .foneFixo(foneFixo)
-           .build();
-   }
+        return Cliente.builder()
+                .usuario(buildUsuario())
+                .nome(nome)
+                .dataNascimento(dataNascimento)
+                .cpf(cpf)
+                .foneCelular(foneCelular)
+                .foneFixo(foneFixo)
+                .build();
+    }
+
+    public Usuario buildUsuario() {
+        return Usuario.builder()
+                .username(email)
+                .password(password)
+                .roles(Arrays.asList(Usuario.ROLE_CLIENTE))
+                .build();
+    }
 
 }
